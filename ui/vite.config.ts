3813881f836f1977f69b4c51d479e4a7ecdee214
hovallev <hovallev@gmail.com>
2025-04-26 13:47:24 -0400

    vercel adjustment

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'public',  // Change output directory to 'public' for Vercel
+  },
   server: {
     port: 3001,
     proxy: {
